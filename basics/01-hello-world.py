34f8432f774a6fcf5b2cd35a1b52a6e001f6b666
BryanAnariba <saariel115@gmail.com>
2022-03-28 22:01:38 -0600

    fist commit with basic sintaxs in python

---
+++ basics/01-hello-world.py
@@ -0,0 +1,11 @@
+def sumar( numero1, numero2 ):
+    return float( numero1 ) + float( numero2 )
+
+def main():
+    print( 'Hello World' )
+    print( 'La suma de los numeros es: ', sumar( 5, 15 ) )
+
+def bucleWithPassAndContinue():
+    pass
+
+main()
