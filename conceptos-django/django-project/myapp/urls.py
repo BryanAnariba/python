24b93f9b2ee38c6f3f8ae318b1ced7589eee3dc5
BryanAnariba <43623601+BryanAnariba@users.noreply.github.com>
2022-12-30 18:03:33 -0600

    Django bAS

--- conceptos-django/django-project/myapp/urls.py
+++ conceptos-django/django-project/myapp/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from .views import hello, about, index, courses, projects, tasks, getTask, getTaskByName
+from .views import hello, about, index, courses, projects, tasks, getTask, getTaskByName, createTask, createProject
 
 urlpatterns = [
-    path('', index),
-    path('about/', about),
-    path('hello/<str:username>', hello), #http://127.0.0.1:8000/api/hello/Bryan
-    path('courses/<int:courseId>', courses),
-    path('projects/', projects),
-    path('tasks/', tasks),
-    path('tasks/<int:taskId>', getTask),
-    path('tasks/<str:taskName>', getTaskByName)
+    path( '', index ),
+    path( 'about/', about ),
+    path( 'hello/<str:username>', hello ), #http://127.0.0.1:8000/api/hello/Bryan
+    path( 'courses/<int:courseId>', courses ),
+    path( 'projects/', projects ),
+    path( 'projects/create/', createProject, name='createproject' ),
+    path( 'tasks/', tasks, name='task'),
+    path( 'tasks/<int:taskId>', getTask),
+    path( 'tasks/create', createTask, name='createtask' ),
+    path( 'tasks/search/<str:taskName>', getTaskByName ),
 ]
--- conceptos-django/django-project/myapp/views.py
+++ conceptos-django/django-project/myapp/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
-
-# 2: 20
+from .forms import CreateNewTask, CreateNewProject
 
 def hello( request, username: str ):
     return HttpResponse(f"<h1>Home { username }</h1>")
@@ -16,14 +15,14 @@ def projects( request ): #http://127.0.0.1:8000/api/projects/
     projectsData = Project.objects.all()
     response: dict = { "status": 200, "projects": projectsData}
     #return JsonResponse( response, status = 200, safe=False )
-    return render( request, 'views/projects.html', { 'data': response })
+    return render( request, 'projects/projects.html', { 'data': response })
 
 def getTask( request, taskId: int ): #http://127.0.0.1:8000/api/tasks/1
     taskData = list(Task.objects.filter(id=taskId).values())
     response: dict = { "status": 200, "data": taskData[0]}
     return JsonResponse( response, status = 200, safe=False )
 
-def getTaskByName( request, taskName: str ):
+def getTaskByName( request, taskName: str ): #http://127.0.0.1:8000/api/tasks/search/Fundamentos JS
     taskData = list( Task.objects.filter( title__icontains=taskName ).values() )
     response: dict = { "status": 200, "data": taskData }
     return JsonResponse( response, status = 200, safe=False )
@@ -31,7 +30,7 @@ def getTaskByName( request, taskName: str ):
 def tasks( request ): #http://127.0.0.1:8000/api/tasks/
     tasks = Task.objects.all()
     response: dict = { 'status': 200, 'tasks': tasks }
-    return render( request, 'views/tasks.html', { 'data': response })
+    return render( request, 'tasks/tasks.html', { 'data': response })
 
 # RENDERIZANDO PLANTILLAS O HTML
 def index( request ): #http://127.0.0.1:8000/api/
@@ -41,6 +40,31 @@ def index( request ): #http://127.0.0.1:8000/api/
 def about( request ): #http://127.0.0.1:8000/api/about
     userName: str = 'bsanchez'
     return render( request, 'views/about.html', { 'userName': userName })
+
+def createTask( request ): #http://127.0.0.1:8000/api/tasks/create?title=BRR&description=BR%0D%0A
+    #print( request.GET[ 'title' ] )
+    #print( request.GET[ 'description' ] )
+    if request.method  == 'GET':
+        return render( request, 'tasks/create-task.html', { 'form': CreateNewTask })
+    else:
+        Task.objects.create(
+            title = request.POST[ 'title' ],
+            description = request.POST[ 'description' ],
+            project_id=1
+        )
+        return redirect( '/api/tasks' )
+
+def createProject( request ):
+    if request.method == 'GET':
+        return render( request, 'projects/create-project.html', { 'form': CreateNewProject } )
+    else:
+        Project.objects.create(
+            name = request.POST[ 'name' ]
+        )
+        return redirect( '/api/projects' )
+
+
+        
 
 
 
