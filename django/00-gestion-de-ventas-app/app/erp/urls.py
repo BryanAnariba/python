b51a81cf050fe46fe43e84bad3fedd5a34b8906e
Bryan Sanchez <saariel115@gmail.com>
2023-09-02 09:17:11 -0600

    Aprendiendo Django Rest Framework despues decido angular o react

--- django/00-gestion-de-ventas-app/app/erp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from erp.views.category.views import category_list, CategoryListView
-
-urlpatterns = [
-    path('categories/', category_list),
-    path('categories/list/', CategoryListView.as_view())
-]
